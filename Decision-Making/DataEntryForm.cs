c84fab7e75133e7ff9f58a25852dd8da90c9cd7d
Alexandru Clapa <alexandru.clapa@gmail.com>
2014-03-27 19:08:32 +0200

    Method to check if data was entered correctly.

--- Decision-Making/DataEntryForm.cs
+++ Decision-Making/DataEntryForm.cs
@@ -41,6 +41,26 @@ namespace Decision_Making
             }
         }
 
+        private bool checkEntryData()
+        {
+            if (this.typeOfAction.SelectedIndex == 0)
+            {
+                if (this.annualFixedCost.Value > 0 && this.variableCostPerUnit.Value > 0 && this.annualVolume > 0)
+                {
+                    return true;
+                }
+                return false;
+            } 
+            else
+            {
+                if (this.annualFixedCost.Value > 0 && this.annualVolume.Value > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
         private void saveButton_Click(object sender, EventArgs e)
         {
 
