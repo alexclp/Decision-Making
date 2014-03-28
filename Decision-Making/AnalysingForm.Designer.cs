03af18ce1b564211253e11b3bb001d1c686613e0
Alexandru Clapa <alexandru.clapa@gmail.com>
2014-03-28 17:42:32 +0200

    redesign in analysis form. more work to be done.

--- Decision-Making/AnalysingForm.Designer.cs
+++ Decision-Making/AnalysingForm.Designer.cs
@@ -28,67 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.buyingCost = new System.Windows.Forms.Label();
-            this.makingCost = new System.Windows.Forms.Label();
+            this.costsGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.costsGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // costsGridView
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(178, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Cost anual pentru cumparare produs";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(71, 102);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(169, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Cost anual pentru fabricare produs";
-            // 
-            // buyingCost
-            // 
-            this.buyingCost.AutoSize = true;
-            this.buyingCost.Location = new System.Drawing.Point(271, 64);
-            this.buyingCost.Name = "buyingCost";
-            this.buyingCost.Size = new System.Drawing.Size(0, 13);
-            this.buyingCost.TabIndex = 2;
-            // 
-            // makingCost
-            // 
-            this.makingCost.AutoSize = true;
-            this.makingCost.Location = new System.Drawing.Point(256, 102);
-            this.makingCost.Name = "makingCost";
-            this.makingCost.Size = new System.Drawing.Size(0, 13);
-            this.makingCost.TabIndex = 3;
+            this.costsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.costsGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.costsGridView.Location = new System.Drawing.Point(24, 23);
+            this.costsGridView.Name = "costsGridView";
+            this.costsGridView.Size = new System.Drawing.Size(574, 150);
+            this.costsGridView.TabIndex = 0;
             // 
             // AnalysingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(626, 373);
-            this.Controls.Add(this.makingCost);
-            this.Controls.Add(this.buyingCost);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.costsGridView);
             this.Name = "AnalysingForm";
             this.Text = "AnalysingForm";
+            ((System.ComponentModel.ISupportInitialize)(this.costsGridView)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label buyingCost;
-        private System.Windows.Forms.Label makingCost;
+        private System.Windows.Forms.DataGridView costsGridView;
+
     }
 }
--- Decision-Making/AnalysingForm.cs
+++ Decision-Making/AnalysingForm.cs
@@ -15,7 +15,54 @@ namespace Decision_Making
         public AnalysingForm()
         {
             InitializeComponent();
-
+            InitializeGridView();
         }
+
+        private void InitializeGridView()
+        {
+            Product product = null;
+
+            try
+            {
+                product = DataSaver.LoadData();
+            }
+
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("Nu exista date!");
+                return;
+            }
+
+            finally
+            {
+                if (product != null)
+                {
+                    string makingTotalCost = MakingCost(product);
+                    string buyingTotalCost = BuyingCost(product);
+
+                    this.costsGridView.Columns.Add("Tip", "Tip");
+                    this.costsGridView.Columns.Add("Cost fix anual", "Cost fix anual");
+                    this.costsGridView.Columns.Add("Cost variabil/unitate", "Cost variabil/unitate");
+                    this.costsGridView.Columns.Add("Volum anual(unitati)", "Volum anual(unitati)");
+                    this.costsGridView.Columns.Add("Cost total anual", "Cost total anual (calculat)");
+
+                    this.costsGridView.Rows.Insert(0, "Fabricare", product.makingAnnualCost, product.makingVariableCost, product.annualVolume, makingTotalCost);
+                    this.costsGridView.Rows.Insert(1, "Cumparare", "0", product.buyingVariableCost, product.annualVolume, buyingTotalCost);
+                }
+            }
+            
+        }
+
+        private string MakingCost(Product product)
+        {
+            decimal result = System.Convert.ToDecimal(product.makingAnnualCost) + System.Convert.ToDecimal(product.annualVolume) * System.Convert.ToDecimal(product.makingVariableCost);
+            return result.ToString();
+        }
+        
+        private string BuyingCost(Product product)
+        {
+            decimal result = 0 + System.Convert.ToDecimal(product.annualVolume) * System.Convert.ToDecimal(product.buyingVariableCost);
+            return result.ToString();
+        }   
     }
 }
